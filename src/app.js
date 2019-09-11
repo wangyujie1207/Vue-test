fabcfab7f03b0b494a019a7354b061f6fc9675f6
wangyujie1207 <82414199@qq.com>
2019-09-11 16:28:25 +0800

    使用chai添加测试用力

--- src/app.js
+++ src/app.js
@@ -14,3 +14,85 @@ new Vue({
     }
   }
 });
+
+import chai from 'chai';
+const expect = chai.expect;
+// 单元测试
+{
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  });
+  vm.$mount(div);
+  let useElement = vm.$el.querySelector('use');
+  expect(useElement.getAttribute('xlink:href')).to.equal('#i-settings');
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings',
+      loading: true
+    }
+  });
+  vm.$mount(div);
+  let useElement = vm.$el.querySelector('use');
+  expect(useElement.getAttribute('xlink:href')).to.equal('#i-loading');
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  });
+  vm.$mount(div);
+  let svgElement = vm.$el.querySelector('svg');
+  let {order} = window.getComputedStyle(svgElement);
+  expect(order).to.equal('1');
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  const div = document.createElement('div');
+  document.body.appendChild(div);
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  });
+  vm.$mount('#test');
+  let divElement = vm.$el.querySelector('div');
+  let {order} = window.getComputedStyle(divElement);
+  expect(order).to.equal('2');
+  vm.$el.remove();
+  vm.$destroy();
+}
+{
+  const Constructor = Vue.extend(Button);
+  const vm = new Constructor({
+    propsData: {
+      icon: 'settings'
+    }
+  });
+  vm.$mount();
+  vm.$on('click', function(){
+    console.log(1)
+  });
+  let button = vm.$el;
+  button.click();
+
+}
