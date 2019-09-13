898d103067c347a72867a4ff0dbdd49c413ffdae
wangyujie1207 <82414199@qq.com>
2019-09-13 16:47:46 +0800

    添加row align

---
+++ src/row.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="row" :style="rowStyle" :class="rowClass">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "g-row",
+    props: {
+      gutter: {
+        type: [Number, String]
+      },
+      align: {
+        type: String,
+        validator(value){
+          return  ['left', 'right','center'].includes(value)
+        }
+      }
+    },
+    computed: {
+      rowClass(){
+        let {align} = this;
+        return [align && `align-${align}`]
+      },
+      rowStyle() {
+        let {gutter} = this;
+        return {marginLeft: -gutter / 2 + 'px', marginRight: -gutter / 2 + 'px'}
+      }
+    },
+    mounted() {
+      this.$children.forEach(vm => {
+        vm.gutter = this.gutter
+      })
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .row {
+    display: flex;
+    &.align-left{
+      justify-content: flex-start;
+    }
+    &.align-right{
+      justify-content: flex-end;
+    }
+    &.align-center{
+      justify-content: center;
+    }
+  }
+</style>
