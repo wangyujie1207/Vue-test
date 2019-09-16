def6de3d49e9bd314d491a703d3758938c9cd088
wangyujie1207 <82414199@qq.com>
2019-09-16 22:59:40 +0800

    完成popover基本功能

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="popover" @click.stop="xxx">
+  <div class="popover" @click="onClick" ref="popover">
     <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
@@ -17,26 +17,41 @@
         visible: false
       }
     },
-    mounted() {
-      this.$nextTick(() => {
-        console.log(this.$refs.triggerWrapper);
-      })
-    },
     methods: {
-      xxx() {
-        this.visible = !this.visible;
-        if (this.visible === true) {
-          this.$nextTick(() => {
-            document.body.appendChild(this.$refs.contentWrapper);
-            let {width, height, left, top} = this.$refs.triggerWrapper.getBoundingClientRect();
-            this.$refs.contentWrapper.style.left = `${left+window.scrollX}px`;
-            this.$refs.contentWrapper.style.top = `${top+window.scrollY}px`;
-            let eventHandler = () => {
-              this.visible = false;
-              document.removeEventListener('click', eventHandler);
-            };
-            document.addEventListener('click', eventHandler);
-          })
+      positionContent () {
+        document.body.appendChild(this.$refs.contentWrapper);
+        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
+        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+      },
+      onClickDocument (e) {
+        if (this.$refs.popover &&
+          (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))
+        ) {
+          console.log('11');
+          return
+        }
+        this.close()
+      },
+      open () {
+        this.visible = true;
+        this.$nextTick(() => {
+          this.positionContent();
+          document.addEventListener('click', this.onClickDocument)
+        })
+      },
+      close () {
+        this.visible = false;
+        console.log('guanbi');
+        document.removeEventListener('click', this.onClickDocument)
+      },
+      onClick (event) {
+        if (this.$refs.triggerWrapper.contains(event.target)) {
+          if (this.visible === true) {
+            this.close()
+          } else {
+            this.open()
+          }
         }
       }
     }
