2d439ba44face6c08d9d3f9ba2ac519c31e293f6
wangyujie1207 <82414199@qq.com>
2019-09-21 10:04:50 +0800

    可以设置默认selected

--- src/collapse.vue
+++ src/collapse.vue
@@ -5,8 +5,32 @@
 </template>
 
 <script>
+  import Vue from 'vue'
+
   export default {
-    name: "g-collapse"
+    name: "g-collapse",
+    props: {
+      single: {
+        type: Boolean,
+        default: false
+      },
+      selected: {
+        type: String,
+      }
+    },
+    data() {
+      return {
+        eventBus: new Vue()
+      }
+    },
+    provide() {
+      return {
+        eventBus: this.eventBus
+      }
+    },
+    mounted() {
+      this.eventBus.$emit('update:selected', this.selected)
+    }
   }
 </script>
 
